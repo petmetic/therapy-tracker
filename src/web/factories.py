241c2eb01ad3f3e31a2d2e775beb86e7e4ce3353
Meta <petmetic@gmail.com>
2023-03-27 12:29:40 +0200

    delete .idea from repo and testing for index page and add new massage

---
+++ src/web/factories.py
@@ -0,0 +1,17 @@
+import factory
+from . import models
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.User
+
+    first_name = factory.Sequence(lambda n: "Agent %03d" % n)
+
+
+class CustomerFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Customer
+
+    name = factory.Faker("name")
+    email = factory.Faker("email")
