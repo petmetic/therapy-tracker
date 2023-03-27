8c032147f02181e5c499cbaada86f4f15735c912
petmetic <101994210+petmetic@users.noreply.github.com>
2023-03-27 12:32:45 +0200

    delete .idea from repo and testing for index page and add new massage (#18)

--- src/web/tests.py
+++ src/web/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .factories import UserFactory, CustomerFactory
+from .models import Massage
+
+
+class IndexTest(TestCase):
+    def test_homepage_loads(self):
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, text="Today's massage:")
+
+    def test_submit_new_massage(self):
+        therapist = UserFactory()
+        customer = CustomerFactory(email="test@example.com")
+        data = {
+            "customer": [customer.id],
+            "therapist": [therapist.id],
+            "massage_date": ["2023-04-12"],
+            "reason_for_visit": ["ali deluje"],
+            "kind": ["terapevtska"],
+            "massage_notes": ["gremo naprej"],
+            "next_visit": ["hahaha"],
+            "recommendations": [""],
+            "personal_notes": [""],
+            "duration": ["30 min"],
+            "amount": ["45"],
+            "discount": ["30 %"],
+            "discount_reason": ["kar tako"],
+            "repeat_visit": ["on"],
+        }
+
+        response = self.client.post(
+            reverse("massage_add", kwargs={"customer_pk": customer.pk}), data=data
+        )
+        massage = Massage.objects.latest("id")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, reverse("massage_detail", kwargs={"pk": massage.pk})
+        )
+
+    def test_check_listing_views(self):
+        pass
+
+    def check_access_permission(self):
+        pass
+
+
+# TODO: factories to create customers, therapists(users) and therapies
