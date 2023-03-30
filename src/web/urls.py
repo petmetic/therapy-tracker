f91c6fa8c02b94a43c979c6454425d8adcf2a195
Meta <petmetic@gmail.com>
2023-03-30 14:09:54 +0200

    reordering urls

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
-    path("logout", views.custom_logout, name="logout"),
-    path("error/", views.error, name="error"),
-    path("", views.index, name="index"),
     path("customer_list/", views.customer_list, name="customer_list"),
     path(
         "customer_list/<int:pk>/detail/", views.customer_detail, name="customer_detail"
     ),
+    path("customer_list/add/", views.customer_add, name="customer_add"),
+    path("error/", views.error, name="error"),
     path("massage_list/<int:pk>/detail/", views.massage_detail, name="massage_detail"),
     path("massage_list/<int:customer_pk>/add/", views.massage_add, name="massage_add"),
-    path("customer_list/add/", views.customer_add, name="customer_add"),
+    path("logout", views.custom_logout, name="logout"),
+    path("", views.index, name="index"),
 ]
