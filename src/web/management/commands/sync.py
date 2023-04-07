d157120930d735609ab4d0f7ae488d06a42bf075
Meta <petmetic@gmail.com>
2023-04-07 11:19:04 +0200

    opend json file

--- src/web/management/commands/sync.py
+++ src/web/management/commands/sync.py
@@ -3,6 +3,7 @@ from ...models import Customer
 from django.conf import settings
 import requests
 import requests_cache
+import json
 
 session = requests_cache.CachedSession("requests_cache")
 
@@ -17,6 +18,12 @@ class Command(BaseCommand):
         r = session.get(
             settings.WP_URL_ENTITIES, auth=(settings.WP_USER, settings.WP_PASSWORD)
         )
-        print(r.json())
+
+        # r2 = session.get(settings.WP_URL_CUSTOMERS, auth=(settings.WP_USER, settings.WP_PASSWORD))
+        with open("/Users/meta/code/hacking/therapy-tracker/users_PP_alenka.json") as f:
+            data = json.loads(f.read())
+
+        print(data)
+
         self.stdout.write(str(Customer.objects.all().count()))
         self.stdout.write(self.style.SUCCESS("Successfully synced"))
