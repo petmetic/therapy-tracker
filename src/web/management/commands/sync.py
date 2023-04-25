8c3051fd0631e5b35320d923a93a756c6acbc27e
Meta <petmetic@gmail.com>
2023-04-25 13:33:29 +0100

    partial sync with wp

--- src/web/management/commands/sync.py
+++ src/web/management/commands/sync.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from ...models import Customer
+from ...models import Customer, User, Massage
 from django.conf import settings
-import requests
 import requests_cache
 import json
+from icecream import ic
 
 session = requests_cache.CachedSession("requests_cache")
 
@@ -20,10 +20,57 @@ class Command(BaseCommand):
         )
 
         # r2 = session.get(settings.WP_URL_CUSTOMERS, auth=(settings.WP_USER, settings.WP_PASSWORD))
-        with open("/Users/meta/code/hacking/therapy-tracker/users_PP_alenka.json") as f:
+        # with open("/Users/meta/code/hacking/therapy-tracker/users_PP_alenka.json") as f:
+        #     data = json.loads(f.read())
+        #     customers = data['data']['users']
+        #
+        #     for raw_customer in customers:
+        #         Customer.objects.create(
+        #             external_id=raw_customer.get('id'),
+        #             name=raw_customer.get('firstName'),
+        #             surname=raw_customer.get('lastName'),
+        #             email=raw_customer.get('email'),
+        #             phone=raw_customer.get('phone')
+        #         )
+        #
+        # self.stdout.write(str(Customer.objects.all().count()))
+        # self.stdout.write(self.style.SUCCESS("Successfully synced customers"))
+        #
+        # with open("/Users/meta/code/hacking/therapy-tracker/therapists_PP_alenka.json") as f:
+        #     data = json.loads(f.read())
+        #     therapists = data['data']['employees']
+        #
+        #     for raw_therapist in therapists:
+        #         User.objects.get_or_create(
+        #             # external_id=raw_therapist.get('id'),
+        #             first_name=raw_therapist.get('firstName'),
+        #             email=raw_therapist.get('email'),
+        #             username=raw_therapist.get('email'),
+        #         )
+        # self.stdout.write(str(User.objects.all().count()))
+        # self.stdout.write(self.style.SUCCESS("Successfully synced therapists"))
+
+        with open(
+            "/Users/meta/code/hacking/therapy-tracker/appointments_PP_alenka.json"
+        ) as f:
             data = json.loads(f.read())
+            massages = data["data"]["appointments"]
 
-        print(data)
+            for dates, appointments in massages.items():
+                for app in appointments["appointments"]:
+                    customer, created = Customer.objects.get_or_create(
+                        external_id=app["bookings"][0]["customer"].get("id"),
+                        name=app["bookings"][0]["customer"].get("firstName"),
+                        surname=app["bookings"][0]["customer"].get("lastName"),
+                        email=app["bookings"][0]["customer"].get("email"),
+                        phone=app["bookings"][0]["customer"].get("phone"),
+                    )
+                    therapist = User.objects.get()
 
-        self.stdout.write(str(Customer.objects.all().count()))
-        self.stdout.write(self.style.SUCCESS("Successfully synced"))
+                    Massage.objects.get_or_create(
+                        massage_date=app.get("bookingStart"),
+                        customer=customer,
+                        status=app.get("status"),
+                        service=app.get("serviceId"),
+                        therapist_id=app.get("providerId"),
+                    )
--- src/web/models.py
+++ src/web/models.py
@@ -12,7 +12,7 @@ class Massage(models.Model):
         on_delete=models.CASCADE,
     )
     massage_date = models.DateTimeField(blank=True, null=True)
-    # needs to be date of current massage; can be changed n request by user
+    # needs to be date of current massage; can be changed on request by user
     # (not necessarily entered the same date as therapy done)
     reason_for_visit = models.TextField(default="")
     kind = models.CharField(max_length=300)
@@ -25,6 +25,8 @@ class Massage(models.Model):
     discount = models.CharField(max_length=200, default="", blank=True)
     discount_reason = models.CharField(max_length=200, default="", blank=True)
     repeat_visit = models.BooleanField(default=False)
+    status = models.CharField(max_length=200, default="", blank=True)
+    service = models.CharField(max_length=200, default="", blank=True)
 
     added = models.DateTimeField(auto_now_add=True)
     changed = models.DateTimeField(auto_now=True)
@@ -35,14 +37,15 @@ class Massage(models.Model):
 
 class Customer(models.Model):
     name = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254)
-    phone = models.CharField(max_length=100)
-    occupation = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200, null=True, default="")
+    email = models.EmailField(max_length=254, null=True, default="")
+    phone = models.CharField(max_length=100, null=True, default="")
+    occupation = models.CharField(max_length=200, null=True, default="")
     previous_massage = models.BooleanField(default=False)
     salon_choice = models.CharField(max_length=200, default="")
     frequency = models.CharField(max_length=200, default="")
     referral = models.CharField(max_length=200, default="")
+    external_id = models.CharField(blank=True, null=True, default="", max_length=50)
 
     def __str__(self):
         return f"{self.surname} {self.name}"
