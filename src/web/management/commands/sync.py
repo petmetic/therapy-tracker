24ff5bf60c12844755fb351def340b0eb362e70b
petmetic <101994210+petmetic@users.noreply.github.com>
2023-05-15 16:17:14 +0200

    Wp sync with real data (#73)
    * Massage in admin view fix
    
    * added dispaly therapist name on homepage and logout button
    
    * added dispaly therapist name display massage
    
    * proof of concept WP sync
    
    * wp sync running, but not automated
    
    * fix if not match print statement

--- src/web/management/commands/sync.py
+++ src/web/management/commands/sync.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from ...models import Customer, User, Massage, Service
-import requests_cache
+import requests
+import re
+from django.conf import settings
 import json
 from ...utility import (
     therapist_import,
@@ -9,31 +11,39 @@ from ...utility import (
     massage_import,
 )
 
-session = requests_cache.CachedSession("requests_cache")
-
 
 class Command(BaseCommand):
     help = "Help to do sync"
 
     def handle(self, *args, **options):
-        # r = session.get(
-        #     settings.WP_URL_ENTITIES, auth=(settings.WP_USER, settings.WP_PASSWORD)
-        # )
+        regex = r"var\s+wpAmeliaNonce\s*=\s*['\"]?([a-fA-F0-9]+)['\"]?"
 
-        # NOT NEEDED ANYMORE, JUST FOR THE API
-        # r2 = session.get(settings.WP_URL_CUSTOMERS, auth=(settings.WP_USER, settings.WP_PASSWORD))
-        # with open("/Users/meta/code/hacking/therapy-tracker/users_PP_alenka.json") as f:
-        #     data = json.loads(f.read())
-        #     customers = data['data']['users']
-        #
-        #
-        with open(
-            "/Users/meta/code/hacking/therapy-tracker/therapists_PP_alenka.json"
-        ) as f:
-            data = json.loads(f.read())
+        with requests.Session() as s:
+            headers1 = {"Cookie": "wordpress_test_cookie=WP Cookie check"}
+            datas = {
+                "log": settings.WP_USER,
+                "pwd": settings.WP_PASSWORD,
+                "wp-submit": "Log In",
+                "redirect_to": settings.WP_ADMIN,
+                "testcookie": "1",
+            }
+            s.post(settings.WP_LOGIN, headers=headers1, data=datas)
+            resp = s.get(settings.WP_ADMIN)
 
-            therapist_import(data)
-            services_import(data)
+            match = re.search(regex, resp.text)
+            if not match:
+                print("No nonce found")
+                return
+            nonce = match.group(1)
+            entities_json = s.get(settings.WP_URL_ENTITIES.format(nonce)).text
+            data_entities = json.loads(entities_json)
+
+            appointments_json = s.get(settings.WP_URL_APPOINTMENTS.format(nonce)).text
+            data_appointments = json.loads(appointments_json)
+
+        # import services, therapists
+        therapist_import(data_entities)
+        services_import(data_entities)
 
         self.stdout.write(str(Service.objects.all().count()))
         self.stdout.write(self.style.SUCCESS("Successfully synced services"))
@@ -41,13 +51,9 @@ class Command(BaseCommand):
         self.stdout.write(str(User.objects.all().count()))
         self.stdout.write(self.style.SUCCESS("Successfully synced therapists"))
 
-        with open(
-            "/Users/meta/code/hacking/therapy-tracker/appointments_PP_alenka.json"
-        ) as f:
-            data = json.loads(f.read())
-
-            customer_import(data)
-            massage_import(data)
+        # import appointments
+        customer_import(data_appointments)
+        massage_import(data_appointments)
 
         self.stdout.write(str(Customer.objects.all().count()))
         self.stdout.write(self.style.SUCCESS("Successfully synced customers"))
