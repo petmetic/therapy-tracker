dc074db6b904ed09358d21265fb4956bddf5852f
Meta <petmetic@gmail.com>
2023-05-16 09:53:11 +0100

    partial login change

--- src/web/tests.py
+++ src/web/tests.py
@@ -22,7 +22,7 @@ class GeneralTest(TestCase):
         response = self.client.get("/logout", follow=True)
 
         self.assertRedirects(response, "/accounts/login/?next=/")
-        self.assertContains(response, text="Please log in to see Alenka masaže.")
+        self.assertContains(response, text="Please log in to see")
 
     def test_homepage_loads(self):  # therapist already logged in
         therapist = UserFactory()
