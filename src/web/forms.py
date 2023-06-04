b5a503a4ba37b0172f2fadf74be2ad56185c23f1
Meta <petmetic@gmail.com>
2023-06-04 15:52:54 +0100

    forms fix

--- src/web/forms.py
+++ src/web/forms.py
@@ -6,7 +6,7 @@ from .models import Massage, Customer
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class MassageForm(ModelForm):
