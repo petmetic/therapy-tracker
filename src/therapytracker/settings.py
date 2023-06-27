360fd7aaea453421a8cf47012f0f9367716e6f1c
petmetic <101994210+petmetic@users.noreply.github.com>
2023-06-27 12:01:20 +0200

    added sentry for error tracking (#112)
    * added sentry for error tracking
    
    * sentry update

--- src/therapytracker/settings.py
+++ src/therapytracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,5 +155,6 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
 
 from .localsettings import *  # noqa: F403,F401,E402
