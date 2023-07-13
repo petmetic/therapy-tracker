19ea55996a83afcfec34c5261ded86b31b1e9287
petmetic <101994210+petmetic@users.noreply.github.com>
2023-07-13 10:40:22 +0200

    adding admin_sudo user to login as any user in Django admin (#121)

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path(r"su/", include("django_su.urls")),
     path("accounts/login/", views.CustomLoginView.as_view(), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("customer_list/", views.customer_list, name="customer_list"),
