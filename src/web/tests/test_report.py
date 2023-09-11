309984f279251fc22263c712bdae48f0471de679
Meta <petmetic@gmail.com>
2023-09-11 09:16:49 +0200

    test for reporting hours - failed last test

--- src/web/tests/test_report.py
+++ src/web/tests/test_report.py
@@ -27,11 +27,12 @@ class ReportTest(TestCase):
         cls.therapist2 = UserFactory(first_name="Charlotte")
         cls.therapist3 = UserFactory(first_name="Mike")
 
-        cls.customer1 = CustomerFactory(name="Jane")
+        cls.customer1 = CustomerFactory(name="Jane", surname="Doe")
         cls.customer2 = CustomerFactory(name="Adam")
         cls.customer3 = CustomerFactory(name="John")
 
-        cls.service1 = ServiceFactory(payout=30)
+        cls.service1 = ServiceFactory(payout=30, name="Massage 50 min", price=100)
+        cls.service2 = ServiceFactory(payout=10, name="Massage 30 min", price=80)
 
         cls.massage1 = MassageFactory(
             therapist=cls.therapist1,
@@ -42,20 +43,21 @@ class ReportTest(TestCase):
         cls.massage2 = MassageFactory(
             therapist=cls.therapist1,
             customer=cls.customer2,
+            service=cls.service2,
             start=datetime(2023, 8, 1, 18, 0, 0).astimezone(tz=tz),
         )
         cls.massage3 = MassageFactory(
             therapist=cls.therapist1,
             customer=cls.customer3,
+            service=cls.service1,
             start=datetime(2023, 8, 10, 17, 0, 0).astimezone(tz=tz),
         )
         cls.massage4 = MassageFactory(
             therapist=cls.therapist1,
             customer=cls.customer1,
+            service=cls.service1,
             start=datetime(2023, 7, 1, 17, 0, 0).astimezone(tz=tz),
         )
-        cls.massage5 = MassageFactory()
-        cls.massage6 = MassageFactory()
 
     def test_reports_page_displays(self):
         self.client.force_login(self.therapist1)
@@ -103,6 +105,7 @@ class ReportTest(TestCase):
 
     def test_report_hours_detail_page_displays_w_correct_date(self):
         # http://127.0.0.1:8000/report_hours_detail/2/?start-date=2023-09-01&end-date=2023-09-30
+        #  4. Sep 2023 ob 14:00	Masten, Igor	Masaža 50 minut	0 eur	25 eur
 
         self.client.force_login(self.therapist1)
 
@@ -110,11 +113,29 @@ class ReportTest(TestCase):
         end_date = "2023-08-31"
 
         response = self.client.get(
-            f"{reverse('report_hours_detail')}?start-date={start_date}&end-date={end_date}",
-            kwargs={"pk": self.therapist.pk},
+            f"{reverse('report_hours_detail', kwargs={'pk': self.therapist2.pk})}?start-date={start_date}&end-date={end_date}"
         )
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertContains(response, text="Alice")
+        self.assertContains(response, text="Charlotte")
+        self.assertContains(response, text="30")
+        self.assertNotContains(response, text="Logout: Charlotte")
+        self.assertContains(response, text="Logout: Alice")
         self.assertContains(response, text="Therapist:")
+        self.assertNotContains(response, text="Mike")
+
+        # date out of range of search (month of August)
+        self.assertNotContains(response, text="1. Jul 2023 at 17:00")
+
+        self.assertContains(response, text="1. Aug 2023 at 17:00")
+        self.assertContains(response, text="Jane")
+        self.assertContains(response, text="100")
+        self.assertContains(response, text="30")
+
+        self.assertContains(response, text="1. Aug 2023 at 18:00")
+        self.assertContains(response, text="Adam")
+        self.assertContains(response, text="80")
+        self.assertContains(response, text="10")
+
+        self.assertContains(response, text="Massage 50 min")
